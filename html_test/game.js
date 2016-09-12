3dec20ed72a57c1f1741eeaedf0e51e37abca6b4
Alex Rich <alexandersrich@gmail.com>
2016-09-12 14:32:53 -0400

    add new game
    basic "breakout" game

--- html_test/game.js
+++ html_test/game.js
@@ -4,15 +4,229 @@
  */
 
 /*jslint browser: true*/
-/*global $, Phaser, window, setTimeout, console*/
+/*global $, Phaser, window, setTimeout, setInterval, clearInterval, console*/
+
+function Game2(popupCreator) {
+    "use strict";
+    var game,
+        preload,
+        create,
+        update,
+        ballLost,
+        releaseBall,
+        ballHitBrick,
+        ballHitPaddle,
+        brickTexture,
+        ballTexture,
+        ball,
+        paddle,
+        bricks,
+        ballOnPaddle = true,
+        score = 0,
+        dead = false,
+        scoreText,
+        introText;
+
+    preload = function () {
+        var graphicBase;
+        graphicBase = game.add.graphics(0, 0);
+        graphicBase.beginFill(0xff0000);
+        graphicBase.drawRect(0, 0, 40, 20);
+        graphicBase.endFill();
+        brickTexture = graphicBase.generateTexture();
+        graphicBase.destroy();
+        graphicBase = game.add.graphics(0, 0);
+        graphicBase.beginFill(0x000000);
+        graphicBase.drawCircle(0, 0, 20);
+        graphicBase.endFill();
+        ballTexture = graphicBase.generateTexture();
+        graphicBase.destroy();
+    };
+
+    create = function () {
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        //  We check bounds collisions against all walls other than the bottom one
+        game.physics.arcade.checkCollision.down = false;
+
+        game.stage.backgroundColor = "#00c5cf";
+
+        bricks = game.add.group();
+        bricks.enableBody = true;
+        bricks.physicsBodyType = Phaser.Physics.ARCADE;
+
+        var brick;
+
+        for (var y = 0; y < 4; y++)
+        {
+            for (var x = 0; x < 15; x++)
+            {
+                brick = bricks.create(20 + (x * 50), 100 + (y * 52), brickTexture);
+                brick.body.bounce.set(1);
+                brick.body.immovable = true;
+            }
+        }
+
+        paddle = game.add.sprite(game.world.centerX, 500, brickTexture);
+        paddle.anchor.setTo(0.5, 0.5);
+
+        game.physics.enable(paddle, Phaser.Physics.ARCADE);
+
+        paddle.body.collideWorldBounds = true;
+        paddle.body.bounce.set(1);
+        paddle.body.immovable = true;
+
+        ball = game.add.sprite(game.world.centerX, paddle.y - 16, ballTexture);
+        ball.anchor.set(0.5);
+        ball.checkWorldBounds = true;
+
+        game.physics.enable(ball, Phaser.Physics.ARCADE);
+
+        ball.body.collideWorldBounds = true;
+        ball.body.bounce.set(1);
+
+        ball.events.onOutOfBounds.add(ballLost, this);
+
+        scoreText = game.add.text(32, 550, "score: 0", { font: "20px Arial", fill: "#ffffff", align: "left" });
+        introText = game.add.text(game.world.centerX, 400, "- click to start -", { font: "40px Arial", fill: "#ffffff", align: "center" });
+        introText.anchor.setTo(0.5, 0.5);
+
+        game.input.onDown.add(releaseBall, this);
+
+        game.paused = true;
+
+    };
+
+    update = function () {
+
+        paddle.x = game.input.x;
+
+        if (paddle.x < 24)
+        {
+            paddle.x = 24;
+        }
+        else if (paddle.x > game.width - 24)
+        {
+            paddle.x = game.width - 24;
+        }
+
+        if (ballOnPaddle)
+        {
+            ball.body.x = paddle.x;
+        }
+        else
+        {
+            game.physics.arcade.collide(ball, paddle, ballHitPaddle, null, this);
+            game.physics.arcade.collide(ball, bricks, ballHitBrick, null, this);
+        }
+
+    };
+
+    releaseBall = function () {
+
+        if (ballOnPaddle)
+        {
+            ballOnPaddle = false;
+            ball.body.velocity.y = -300;
+            ball.body.velocity.x = -75;
+            introText.visible = false;
+        }
+
+    };
+
+    ballLost = function () {
+        game.paused = true;
+        dead = true;
+        popupCreator.run(function () {
+            game.paused = false;
+            ballOnPaddle = true;
+            ball.reset(paddle.body.x + 16, paddle.y - 16);
+            dead = false;
+        });
+    };
+
+    ballHitBrick = function (_ball, _brick) {
+
+        _brick.kill();
+
+        score += 10;
+
+        scoreText.text = "score: " + score;
+
+        //  Are they any bricks left?
+        if (bricks.countLiving() == 0)
+        {
+            //  New level starts
+            score += 1000;
+            scoreText.text = "score: " + score;
+            introText.text = "- Next Level -";
+
+            //  Let's move the ball back to the paddle
+            ballOnPaddle = true;
+            ball.body.velocity.set(0);
+            ball.x = paddle.x + 16;
+            ball.y = paddle.y - 16;
+
+            //  And bring the bricks back from the dead :)
+            bricks.callAll("revive");
+        }
+
+    };
+
+    ballHitPaddle = function (_ball, _paddle) {
+
+        var diff = 0;
+
+        if (_ball.x < _paddle.x)
+        {
+            //  Ball is on the left-hand side of the paddle
+            diff = _paddle.x - _ball.x;
+            _ball.body.velocity.x = (-10 * diff);
+        }
+        else if (_ball.x > _paddle.x)
+        {
+            //  Ball is on the right-hand side of the paddle
+            diff = _ball.x -_paddle.x;
+            _ball.body.velocity.x = (10 * diff);
+        }
+        else
+        {
+            //  Ball is perfectly in the middle
+            //  Add a little random X to stop it bouncing straight up!
+            _ball.body.velocity.x = 2 + Math.random() * 8;
+        }
+
+    };
+
+    this.setup = function () {
+        game = new Phaser.Game(800, 600, Phaser.AUTO, "game", {preload: preload, create: create, update: update});
+    };
+
+    this.run = function (time, callback) {
+        game.paused = false;
+        if (dead) {
+            ballOnPaddle = true;
+            ball.reset(paddle.body.x + 16, paddle.y - 16);
+            dead = false;
+        }
+        setTimeout(function () {
+            if (popupCreator.isUp()) {
+                popupCreator.clear();
+            }
+            game.paused = true;
+            callback();
+        }, time * 1000);
+    };
+}
 
 
 function Game(params) {
     "use strict";
     var game,
         state,
-        respawnLength = 2000,
-        postGameFn = params.postGameFn;
+        graphicBase,
+        respawnLength = 2000;
 
     state = {
         preload: function () {
